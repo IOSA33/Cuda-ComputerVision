7db56d16ef4af9c83a0f90f22164cd0cf7654ef3
IOSA33 <sedinkin33@gmail.com>
2026-04-15 20:34:12 +0300

    added rectangle over hand

--- main.cpp
+++ main.cpp
@@ -68,7 +68,7 @@ void HandVision(int width, int height, int channels, unsigned char* data) {
     unsigned char colG{ 138 };
     unsigned char colB{ 116 };
 
-    unsigned char tolerance { 25 };
+    unsigned char tolerance { 20 };
 
     for (size_t y { 0 }; y < height; ++y) {
         for (size_t x { 0 }; x < width; ++x) {
@@ -106,19 +106,52 @@ void calculateCentroid(int width, int height, int channels, unsigned char* data)
     size_t x_coord { sum_10 / sum_00 };
     size_t y_coord { sum_01 / sum_00 };
 
-    std::cout << x_coord << " " << y_coord << '\n';
+    size_t index_centroid { (y_coord * width + x_coord) * channels }; 
 
-    size_t index { (y_coord * width + x_coord) * channels };
+    int r = 1100;
+    int d = 1350;
 
-    for (size_t i { 0 }; i < width; i += 3) {
-        data[index + i] = 255;       
+    for (int dx = -r; dx <= r; ++dx) {
+        int nx = x_coord + dx;
+
+        int ny1 = y_coord - d;
+        int ny2 = y_coord + d;
+
+        if (nx >= 0 && nx < width) {
+            if (ny1 >= 0 && ny1 < height) {
+                size_t index = (ny1 * width + nx) * channels;
+                data[index] = 255;
+            }
+            if (ny2 >= 0 && ny2 < height) {
+                size_t index = (ny2 * width + nx) * channels;
+                data[index] = 255;
+            }
+        }
+    }
+
+    for (int dy = -d; dy <= d; ++dy) {
+        int ny = y_coord + dy;
+
+        int nx1 = x_coord - r;
+        int nx2 = x_coord + r;
+
+        if (ny >= 0 && ny < height) {
+            if (nx1 >= 0 && nx1 < width) {
+                size_t index = (ny * width + nx1) * channels;
+                data[index] = 255;
+            }
+            if (nx2 >= 0 && nx2 < width) {
+                size_t index = (ny * width + nx2) * channels;
+                data[index] = 255;
+            }
+        }
     }
 }
 
 int main() {
     const auto start { std::chrono::high_resolution_clock::now() };
     int width, height, channels;
-    unsigned char* data { stbi_load("../photos/sample2_hand.jpg", &width, &height, &channels, 0) };
+    unsigned char* data { stbi_load("../photos/sample1.jpg", &width, &height, &channels, 0) };
     if (!data) {
         std::cout << "Failed to load INPUT image\n";
         return 1;
